674f09c10c9d255a550438d28f1d68f347ef9a02
brentli1 <brent.jlinville@gmail.com>
2017-10-06 08:25:28 -0500

    add base layout as example; use composer autoload instead of custom autoloader

--- public/index.php
+++ public/index.php
@@ -2,14 +2,6 @@
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-spl_autoload_register(function ($class) {
-    $root = dirname(__DIR__);
-    $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
-    if (is_readable($file)) {
-        require $file;
-    }
-});
-
 $router = new Core\Router();
 
 // Add routes here
